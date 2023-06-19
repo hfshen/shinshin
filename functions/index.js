14459b2e904460ac4866dc9d086213c0e78f76b6
hfshen <hfshen@korea.ac.kr>
2023-06-19 21:06:19 +0900

    function_firebase_api

---
+++ functions/index.js
@@ -0,0 +1,25 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+const functions = require("firebase-functions");
+
+exports.myFunction = functions.https.onRequest((request, response) => {
+  // request.body에 있는 데이터를 받아서 처리하는 코드를 여기에 작성하세요.
+  response.send("Success");
+});
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
